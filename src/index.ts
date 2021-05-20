56fdb5ce6c256cf6584974bccd52421ec8b7fa99
Sylvance Mbaka <kerandisylvance@gmail.com>
2021-05-20 15:27:42 +0300

    Initial files

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { MikroORM } from "@mikro-orm/core";
+import { Post } from "./entities/Post";
+import mikroOrmConfig from "./mikro-orm.config";
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+  app.get('/', (_, res) => {
+    res.send("Hello");
+  });
+  app.listen(4000, () => {
+    console.log("Server started at 4000")
+  });
+
+  const post = orm.em.create(Post, { title: 'Hello Post!' });
+  await orm.em.persistAndFlush(post);
+
+  const posts = await orm.em.find(Post, {});
+  console.log(posts)
+};
+
+main().catch((err) => {
+  console.error(err);
+});
+
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,26 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import path from "path";
+
+export default {
+  debug: !__prod__,
+  dbName: "lite-reddit",
+  entities: [Post],
+  host: 'localhost',
+  migrations: {
+    tableName: 'lite-reddit-migrations', // name of database table with log of executed transactions
+    path: path.join(__dirname, './migrations'), // path to the folder with migrations
+    pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+    transactional: true, // wrap each migration in a transaction
+    disableForeignKeys: true, // wrap statements with `set foreign_key_checks = 0` or equivalent
+    allOrNothing: true, // wrap all migrations in master transaction
+    dropTables: true, // allow to disable table dropping
+    safe: false, // allow to disable table and column dropping
+    emit: 'ts', // migration generation mode
+  },
+  password: "",
+  port: 5432,
+  type: "postgresql",
+  user: "Kerandi"
+} as Parameters<typeof MikroORM.init>[0];
