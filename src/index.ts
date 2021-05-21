20293a5b072226ceb7ac47edf15126d167ac9b89
Sylvance Mbaka <kerandisylvance@gmail.com>
2021-05-21 20:51:22 +0300

    Add more packages

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,26 @@ import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
 
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [],
+      validate: false,
+    })
+  });
+  
   app.get('/', (_, res) => {
     res.send("Hello");
   });
+
   app.listen(4000, () => {
     console.log("Server started at 4000")
   });
